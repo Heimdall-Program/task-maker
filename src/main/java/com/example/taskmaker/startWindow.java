5242884d85f759bdbc500cb8601ee00e216d5100
Даниил <90754791+Heimdall-Program@users.noreply.github.com>
2023-02-05 03:26:03 +0300

    init

--- src/main/java/com/example/taskmaker/startWindow.java
+++ src/main/java/com/example/taskmaker/startWindow.java
@@ -3,7 +3,7 @@ package com.example.taskmaker;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class startWindow {
     @FXML
     private Label welcomeText;
 
